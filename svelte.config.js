34b1a25126b3fd9886571c7314f740d162318ded
Chef <cheftech@pinchepoutine.digital>
2024-06-13 12:45:58 -0600

    add cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,6 @@ export default {
 				include: ['/*'],
 				exclude: ['<all>']
 			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
 		})
 	}
 };
