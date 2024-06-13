cbf1b86ac15898fa7bd364f7778ae647cc550cfe
Chef <cheftech@pinchepoutine.digital>
2024-06-13 12:52:28 -0600

    add cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,6 @@
-import adapter from '@sveltejs/adapter-cloudflare`';
-
-export default {
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-		})
-	}
-};
+import adapter from '@sveltejs/adapter-auto';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {		
+		adapter: adapter()	}};
+export default config;
